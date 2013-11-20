8339d427b03613e02df08b39a6d06b6d2ec30a15
Enrique90m <enrique_90m@hotmail.com>
2013-11-19 23:00:09 -0600

    AgregarFalla_FORM-Agrego metodo para enviar correo, falta perfeccionar la estructura del mismo, pero ya envia correctamente y sin problemas

--- SCM/AgregarFalla.cs
+++ SCM/AgregarFalla.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Net.Mail;
+using System.Net.Mime;
 
 namespace SCM
 {
@@ -37,7 +39,53 @@ namespace SCM
                 objetoFalla.categoria = radioButton2.Text;
           
              dl.AgregaFalla(objetoFalla);
+
+
+            //envia correo    
+             MailMessage mail = new MailMessage();
+             SmtpClient SmtpServer = new SmtpClient();
+             SmtpServer.Credentials = new System.Net.NetworkCredential("enrique.19m@gmail.com", "elkike_9");
+             SmtpServer.Port = 587;
+             SmtpServer.Host = "smtp.gmail.com";
+             SmtpServer.EnableSsl = true;
+
+             try
+             {
+                 mail.From = new MailAddress("enrique.19m@gmail.com", "SCRF - Registro de falla: "+ objetoFalla.numFalla , System.Text.Encoding.UTF8);
+                 mail.To.Add("enrique_90m@hotmail.com");
+                 mail.Subject = "Notificacion de falla";
+                 mail.Body = " <div align=\"center\" style=\"border:2px solid blue\"><h2>Nueva falla registrada en el sistema</h2></div></br></br>";
+                 mail.Body += "<div aling=\"left\"> <h3><b> Numero de falla: </b> "+ objetoFalla.numFalla + "</h3></div></br>";
+                 mail.Body += "<div aling=\"left\"> <h3><b> Numero de equipo: </b>" + objetoFalla.NumComputadora + "</h3></div></br>";
+                 mail.Body += "<div aling=\"left\"> <h3><b> Descripcion de la falla: </b> " + objetoFalla.descripcionFalla + "</h3></div></br>";
+                 mail.Body += "<div aling=\"left\"> <h3><b> Categoria: </b> " + objetoFalla.categoria + "</h3></div></br>";
+                   //if (textBox2.Text != "")
+                 //{
+                 //    LinkedResource logo = new LinkedResource(textBox2.Text);
+                 //    logo.ContentId = "Logo";
+                 //    string htmlview;
+                 //    htmlview = "<html><body><table border=2><tr width=100%><td><img src=cid:Logo alt=companyname /></td><td></td></tr></table><hr/></body></html>";
+                 //    AlternateView alternateView1 = AlternateView.CreateAlternateViewFromString(htmlview + "", null, MediaTypeNames.Text.Html);
+                 //    alternateView1.LinkedResources.Add(logo);
+                 //    mail.AlternateViews.Add(alternateView1);
+                 //}
+                 mail.IsBodyHtml = true;
+                 mail.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+                 //mail.ReplyTo = new MailAddress(TextBox1.Text);
+                 // SmtpServer.Send(mail);
+                 //mail.ReplyTo = new MailAddress(TextBox1.Text);
+                 SmtpServer.Send(mail);
+             }
+             catch (Exception ex)
+             {
+                 MessageBox.Show("No se pudo enviar correo electronico , le presentamos algunas sugerencias y probables motivos de falla :\n \n-Verifique que tenga conexion a internet \n-Actualize el certificado de la pagina ", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                 return;
+             }
+
+             MessageBox.Show("Correo enviado correctamente!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
+
+        
 
         private void button1_Click(object sender, EventArgs e)
         {
